d3dd9135dbd21e49c67d7faa8c10f445f44cd19e
Roberto Matheus Pinheiro Pereira <ro.ber.to1@hotmail.com>
2018-01-30 22:09:29 +0100

    update

--- snakeai/agent/dqn.py
+++ snakeai/agent/dqn.py
@@ -83,19 +83,6 @@ class DeepQNetworkAgent(AgentBase):
         exploration_rate = max_exploration_rate
 
         for episode in range(num_episodes):
-            if env.snake:
-                print (env.foodspeed, env.stats)  # , env.fruit, env.initial_snake_length, env.rewards, env.max_step_limit,
-            # env.is_game_over, env.output, env.timestep_index, env.current_action, env.stats, env.verbose, env.debug_file,
-            # env.stats_file, env.config)
-
-            env2 = env.clone()
-            if env2.snake:
-                print (env2.foodspeed, env2.stats)  # , env2.fruit, env2.initial_snake_length, env2.rewards,
-            # env2.max_step_limit,
-            # env2.is_game_over, env2.output, env2.timestep_index, env2.current_action, env2.stats, env2.verbose,
-            # env2.debug_file,
-            # env2.stats_file, env2.config)
-
             # Reset the environment for the new episode.
             timestep = env.new_episode()
             self.begin_episode()
