db8d483a650a2831353b6f73c514ebe4962b13db
mingyip <fypcheung@gmail.com>
2018-02-02 13:38:52 +0100

    ddqn config

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     MEMORY_SIZE = 100000
     NUM_LAST_FRAMES = 4
     LEVEL = "snakeai/levels/10x10-blank.json"
-    NUM_EPISODES = 100
+    NUM_EPISODES = 10000
     BATCH_SIZE = 64
     DISCOUNT_FACTOR = 0.95
     USE_PRETRAINED_MODEL = False
@@ -13,7 +13,7 @@ class Config:
     MIN_EXPLORATION = 0.1
     # Either sarsa, dqn, ddqn
     LEARNING_METHOD = "ddqn"
-    MULTI_STEP_REWARD = True
+    MULTI_STEP_REWARD = False
     #foodspeed =0 no movement. foodspeed =2 food moves one step every 2 timesteps
     FOODSPEED = 0
-    LOG_FREQUENCY = 200
+    LOG_FREQUENCY = 10
